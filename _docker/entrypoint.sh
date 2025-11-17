14177757cff91c5e45accd4ecfe5307bd6af8b4d
Ahmed Spahić <ahmed.spahic.23@size.ba>
2025-11-17 01:58:23 +0100

    Remove PHP version specification from entrypoint

--- _docker/entrypoint.sh
+++ _docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-# Start PHP-FPM in background (php-fpm8.0, using PHP_VERSION env)
-php-fpm${PHP_VERSION} &
+# Start PHP-FPM in background
+php-fpm &
 
 # Start Nginx in foreground (keeps the container running)
 nginx -g 'daemon off;'
